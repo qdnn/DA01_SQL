a8328c9faf6356ccff059997a18d44931dbce8d1
qdnn <nhuquynh20013110@gmail.com>
2024-10-04 00:48:01 +0700

    Update Project_2.sql

--- Project_2.sql
+++ Project_2.sql
@@ -84,20 +84,47 @@ ORDER BY ROUND(SUM(a.retail_price), 2) DESC;
 
 --- Dashboard
 WITH vw_ecommerce_analyst AS 
-(SELECT FORMAT_DATE('%m', a.created_at) AS month, FORMAT_DATE('%Y', a.created_at) AS year,
+(SELECT FORMAT_DATE('%Y-%m', a.created_at) AS month, FORMAT_DATE('%Y', a.created_at) AS year,
 c.category AS Product_category,
-SUM(b.sale_price) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)) AS TPV,
-COUNT(b.order_id) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)) AS TPO,
-SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)) AS Total_cost,
-(SUM(b.sale_price) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)) - SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at))) AS Total_profit,
-(SUM(b.sale_price) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)) - SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)))/SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%m', a.created_at), FORMAT_DATE('%Y', a.created_at)) AS Profit_to_cost_ratio
+SUM(b.sale_price) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category) AS TPV,
+COUNT(b.order_id) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category) AS TPO,
+SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category) AS Total_cost,
+(SUM(b.sale_price) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category) - SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category)) AS Total_profit,
+(SUM(b.sale_price) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category) - SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category))/SUM(c.cost) OVER (PARTITION BY FORMAT_DATE('%Y-%m', a.created_at), c.category) AS Profit_to_cost_ratio
 FROM bigquery-public-data.thelook_ecommerce.orders AS a
 JOIN bigquery-public-data.thelook_ecommerce.order_items AS b ON a.order_id = b.order_id
 JOIN bigquery-public-data.thelook_ecommerce.products AS c ON b.id = c.id
-ORDER BY month, year)
-SELECT *, LEAD(TPV) OVER (PARTITION BY year ORDER BY month) - TPV AS Revenue_growth,
-LEAD(TPO) OVER (PARTITION BY year ORDER BY month) - TPO AS Order_growth
-FROM vw_ecommerce_analyst;
+ORDER BY month, year),
+new_dataset AS
+(SELECT *, LEAD(TPV) OVER (PARTITION BY year ORDER BY month) - TPV AS Revenue_growth,
+LEAD(TPO) OVER (PARTITION BY year ORDER BY month) - TPO AS Order_growth,
+MIN(month) OVER (PARTITION BY year) AS first_day
+FROM vw_ecommerce_analyst),
+
+-- Tạo retention cohort analysis
+cohort_data AS
+(SELECT *,
+    ((CAST(LEFT(month, 4) AS INT64) - CAST(LEFT(first_day, 4) AS INT64)) * 12) + 
+    (CAST(SUBSTRING(month, 6) AS INT64) - CAST(SUBSTRING(first_day, 6) AS INT64)) AS index
+FROM 
+    new_dataset),
+
+cohort_revenue AS
+(SELECT month,
+    SUM(CASE WHEN index = 0 THEN TPV ELSE 0 END) AS m0,
+    SUM(CASE WHEN index = 1 THEN TPV ELSE 0 END) AS m1,
+    SUM(CASE WHEN index = 2 THEN TPV ELSE 0 END) AS m2,
+    SUM(CASE WHEN index = 3 THEN TPV ELSE 0 END) AS m3
+FROM cohort_data
+GROUP BY month)
+
+-- Retention cohort
+SELECT month,
+ROUND(100.00* m0/m0, 2) ||'%' AS m0,
+ROUND(100.00* m1/m0, 2) ||'%' AS m1,
+ROUND(100.00* m2/m0, 2) ||'%' AS m2,
+ROUND(100.00* m3/m0, 2) ||'%' AS m3
+FROM cohort_revenue;
 
 
 
